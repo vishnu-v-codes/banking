4795ec019937d4fdab6c8dc46ba2a06b7a821f40
Vishnu V <vishnu.v.fullstack@gmail.com>
2025-03-05 17:36:16 +0530

    implement HeaderBox and TotalBalanceBox

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,29 @@
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
 
-const page = () => {
-  return <div>Home</div>;
+const Home = () => {
+    const loggedIn = {firstName: "Vishnu"}
+  return(
+    <section className="home">
+        <div className="home-content">
+            <header className="home-header">
+                <HeaderBox
+                type='greeting'
+                title="Welcome"
+                user={loggedIn?.firstName || "Guest"}
+                subtext="Accesss and manage your account and transactions efficiently."
+                />
+
+                <TotalBalanceBox
+                    accounts={[]}
+                    totalBanks={1}
+                    totalCurrentBalance={1250.35}
+                />
+            </header>
+        </div>
+    </section>
+  )
 };
 
-export default page;
+export default Home;
