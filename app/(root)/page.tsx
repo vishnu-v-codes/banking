6ab5dfae51502f2bacd7e849025c003babd4408f
Vishnu V <vishnu.v.fullstack@gmail.com>
2025-03-11 13:58:25 +0530

    Implement the complete layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,9 @@
 import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
-import React from "react";
 
 const Home = () => {
-    const loggedIn = {firstName: "Vishnu"}
+    const loggedIn = {firstName: "Vishnu", lastName: 'V', email: 'vishnuvofficial@gmail.com'}
   return(
     <section className="home">
         <div className="home-content">
@@ -21,7 +21,15 @@ const Home = () => {
                     totalCurrentBalance={1250.35}
                 />
             </header>
+
+            RECENT TRANSACTIONS
         </div>
+        
+        <RightSidebar 
+            user={loggedIn} 
+            transactions={[]}
+            banks={[{currentBalance: 123.50},{currentBalance: 500}]}
+        />
     </section>
   )
 };
